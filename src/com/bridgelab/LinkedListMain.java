48a4b61e0becde7c8fa682473f24570ea8663b61
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-07-22 00:53:59 +0530

    Added: Added 30 & 56 To 70

--- src/com/bridgelab/LinkedListMain.java
+++ src/com/bridgelab/LinkedListMain.java
@@ -1,22 +1,11 @@
 package com.bridgelab;
 
+
 public class LinkedListMain {
 	public static void main(String[] args) {
-		Node firstNode=new Node(56);
-		Node secondNode=new Node(30);
-		Node thirdNode=new Node(70);
-		
-		Node head = firstNode;
-		firstNode.next=secondNode;
-		secondNode.next=thirdNode;
-		
-		Node temp=head;
-		while(temp!=null)
-		{
-			System.out.println(temp.data);
-			temp=temp.next;
-		}
-		
-
+		System.out.println("Add the data at the start");
+		Operations.addDataAtStart();
 	}
 }
+
+

b88411d80490b8fee066a5e9b730fc0f26a64392
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-07-21 17:16:39 +0530

    Added: Created A Simple LinkedList

--- src/com/bridgelab/LinkedListMain.java
+++ src/com/bridgelab/LinkedListMain.java
@@ -2,8 +2,21 @@ package com.bridgelab;
 
 public class LinkedListMain {
 	public static void main(String[] args) {
-		System.out.println("Welcome To Linked List Data Structure");
-	}
-	
+		Node firstNode=new Node(56);
+		Node secondNode=new Node(30);
+		Node thirdNode=new Node(70);
+		
+		Node head = firstNode;
+		firstNode.next=secondNode;
+		secondNode.next=thirdNode;
+		
+		Node temp=head;
+		while(temp!=null)
+		{
+			System.out.println(temp.data);
+			temp=temp.next;
+		}
+		
 
+	}
 }
